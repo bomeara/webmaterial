f3001785d1fcfd0250830374e8f002d2acef6566
Brian OMeara <omeara.brian@gmail.com>
2016-07-28 17:49:59 -0400

    fix for header issues

--- MakeCV.R
+++ MakeCV.R
@@ -2,3 +2,8 @@ library(cv)
 CreateMarkdown()
 FinalCompileCV()
 system(paste("cp ", tempdir(), "/OM* .", sep=""))
+
+#Stupid. But otherwise the header class for tables causes problems with the theme
+x <- readLines("OMearaCV.html")
+y <- gsub( '<tr class="header">', '<tr class="tableheader">', x )
+cat(y, file="OMearaCV.html", sep="\n")
--- MakeHTML.R
+++ MakeHTML.R
@@ -6,3 +6,11 @@ markdown.files <- system("ls -1 *.md", intern=TRUE)
 for (i in sequence(length(markdown.files))) {
   system(paste("pandoc ", markdown.files[i], " -o ", gsub(".md", ".html", markdown.files[i]), sep=""))
 }
+html.files <- system("ls -1 *.html", intern=TRUE)
+for( f in html.files ){
+
+  x <- readLines(f)
+  y <- gsub( '<tr class="header">', '<tr class="tableheader">', x )
+  cat(y, file=f, sep="\n")
+
+}
